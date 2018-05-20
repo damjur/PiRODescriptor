f7218b20e00eac99f6611fe0c0c60dfcd594a31a
Damian Jurga <damian.jurga@bccgroup.com>
2018-05-20 17:43:11 +0200

    Finished testing script

---
+++ DescriptorTester.py
@@ -0,0 +1,317 @@
+
+# coding: utf-8
+
+
+from bs4 import BeautifulSoup
+import urllib
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+from tqdm import tqdm
+
+import os
+import shutil
+
+from descriptor import extract, distance, D
+
+# In[42]:
+
+
+HOWMANY = 2
+MAX_PATCH_SIZE = 64
+N_POINTS = 2
+DEBUG = False
+URL = "https://www.cs.toronto.edu/~vmnih/data/mass_roads/train/sat/index.html"
+
+
+# In[29]:
+
+
+def generatePointsForOne(image):
+	height,width,_ = image.shape
+	height -= MAX_PATCH_SIZE/2
+	width -= MAX_PATCH_SIZE/2
+	ys = np.random.randint(MAX_PATCH_SIZE/2,height,size=(N_POINTS))
+	xs = np.random.randint(MAX_PATCH_SIZE/2,width,size=(N_POINTS))
+	return [np.dstack((ys,xs))[0]]
+
+def generatePoints(images):
+	points = []
+	for image in images:
+		points += generatePointsForOne(image)
+	return np.array(points)
+
+def getRotats(images,points):
+	r = []
+	ones = np.ones(shape=(N_POINTS, 1))
+	
+	print("Generating rotated images")
+	for a in tqdm([0,1,2,3,5,10,15,30,60,90,180]):
+		for img,pp in zip(images,points):
+			COLS,ROWS,_ =  img.shape
+			M = cv2.getRotationMatrix2D((COLS//2,ROWS//2),a,1)
+			cos = np.abs(M[0, 0])
+			sin = np.abs(M[0, 1])
+ 
+			# compute the new bounding dimensions of the image
+			nW = int((COLS * sin) + (ROWS * cos))
+			nH = int((COLS * cos) + (ROWS * sin))
+ 
+			# adjust the rotation matrix to take into account translation
+			M[0, 2] += (nW / 2) - ROWS//2
+			M[1, 2] += (nH / 2) - COLS//2
+			
+			rimg = cv2.warpAffine(img,M,(nW,nH))
+			rpp = M.dot(np.hstack([pp, ones]).T).T
+			r   += [{"img":rimg,"points":rpp}]
+	return r
+			
+
+def getScales(images,points):
+	r = []
+	
+	print("Generating scaled images")
+	for img,pp in zip(images,points):
+		r   += [{"img":img,"points":pp}]
+		
+	for s in tqdm(np.arange(1.25,3.75,0.25)):
+		for img,pp in zip(images,points):
+			rimg = cv2.resize(img,None,fx=s,fy=s)
+			rpp = s*pp
+			r   += [{"img":rimg,"points":rpp}]
+	return r
+
+def getBlures(images,points):
+	r = []
+	
+	print("Generating blured images")
+	for img,pp in zip(images,points):
+		r   += [{"img":img,"points":pp}]
+	
+	for b in tqdm(np.arange(1,10)):
+		for img,pp in zip(images,points):
+			rimg = cv2.GaussianBlur(img,(5,5),b)
+			r   += [{"img":rimg,"points":pp}]
+	return r
+
+def getJpgeds(images,points):
+	r = []
+	dirname = "tmp"
+	label = "file{}_{}.jpeg"
+	if os.path.isdir(dirname) and DEBUG:
+		shutil.rmtree(dirname) 
+	os.mkdir(dirname)
+	
+	print("Generating images compressed using JPEG algorithm")
+	for q in tqdm(np.arange(0,101,10)):
+		for i,(img,pp) in enumerate(zip(images,points)):
+			filename = os.path.join(dirname, label.format(i,q))
+			cv2.imwrite(filename, img, [cv2.IMWRITE_JPEG_QUALITY, i])
+			rimg = cv2.imread(filename)
+			r   += [{"img":rimg,"points":pp}]
+	shutil.rmtree(dirname) 
+	return r   
+
+def getTransformations(images,points):
+	x = {}
+	x["rotat"] = getRotats(images,points)
+	x["scale"] = getScales(images,points)
+	x["blure"] = getBlures(images,points)
+	x["jpged"] = getJpgeds(images,points)
+	return x
+
+
+# In[30]:
+
+
+def getData(t):
+	epsilon = 0.05
+	X = {}
+	Y = {}
+	
+	if "blure" in t:
+		rds = []
+		print("Extracting descriptors (blured)")
+		for i in tqdm(t["blure"]):
+			rds += [extract(i["img"],i["points"])]
+		r0 = 0
+		rx  = np.arange(0,10)
+		
+		rds = np.array(rds).reshape((HOWMANY,len(rx),N_POINTS,D))
+		rd0 = rds[r0,:]
+		ry  = np.zeros(len(rx))
+		print("Extracting distances (blured)")
+		for i,img in tqdm(enumerate(rds)):
+			cimg = rd0[i]
+			for k,angle in enumerate(img):
+				for j,d1 in enumerate(cimg):
+					row = []
+					for d2 in img:
+						row += [distance(d1,d2)]
+					ry[k] += (np.array(row) <= row[j]*(1+epsilon)).sum() - 1
+		X["blure"] = rx
+		Y["blure"] = ry
+	
+	if "jpged" in t:
+		rds = []
+		print("Extracting descriptors (JPEG compression)")
+		for i in tqdm(t["jpged"]):
+			rds += [extract(i["img"],i["points"])]
+		r0 = 0
+		rx  = np.arange(0,101,10)
+		rds = np.array(rds).reshape((HOWMANY,len(rx),N_POINTS,D))
+		rd0 = rds[r0,:]
+		ry  = np.zeros(len(rx))
+		print("Extracting distances (JPEG compression)")
+		for i,img in tqdm(enumerate(rds)):
+			cimg = rd0[i]
+			for k,angle in enumerate(img):
+				for j,d1 in enumerate(cimg):
+					row = []
+					for d2 in img:
+						row += [distance(d1,d2)]
+					ry[k] += (np.array(row) <= row[j]*(1+epsilon)).sum() - 1
+		X["jpged"] = rx
+		Y["jpged"] = ry
+		
+	if "rotat" in t:
+		r0 = 0
+		rds = []
+		print("Extracting descriptors (rotation)")
+		for i in tqdm(t["rotat"]):
+			rds += [extract(i["img"],i["points"])]
+		rx  = [0,1,2,3,5,10,15,30,60,90,180]
+		rds = np.array(rds).reshape((HOWMANY,len(rx),N_POINTS,D))
+		rd0 = rds[r0,:]
+		ry  = np.zeros(len(rx))
+		print("Extracting distances (rotation)")
+		for i,img in tqdm(enumerate(rds)):
+			cimg = rd0[i]
+			for k,angle in enumerate(img):
+				for j,d1 in enumerate(cimg):
+					row = []
+					for d2 in img:
+						row += [distance(d1,d2)]
+					ry[k] += (np.array(row) <= row[j]*(1+epsilon)).sum() - 1
+		X["rotat"] = rx
+		Y["rotat"] = ry
+	
+	if "scale" in t:
+		rds = []
+		print("Extracting descriptors (scale)")
+		for i in tqdm(t["scale"]):
+			rds += [extract(i["img"],i["points"])]
+		r0 = 0
+		rx  = np.arange(1.25,3.75,0.25)
+		rx = np.append([1],rx)
+		rds = np.array(rds).reshape((HOWMANY,len(rx),N_POINTS,D))
+		rd0 = rds[r0,:]
+		ry  = np.zeros(len(rx))
+		print("Extracting distances (scale)")
+		for i,img in tqdm(enumerate(rds)):
+			cimg = rd0[i]
+			for k,angle in enumerate(img):
+				for j,d1 in enumerate(cimg):
+					row = []
+					for d2 in img:
+						row += [distance(d1,d2)]
+					ry[k] += (np.array(row) <= row[j]*(1+epsilon)).sum() - 1
+		X["scale"] = rx
+		Y["scale"] = ry
+	
+	
+	return X,Y
+
+def drawPlots(X,Y,L):
+	filename = "plot_{}.png"
+	for key in ["rotat","scale","blure","jpged"]:
+		if key in X and key in Y and key in L:
+			plt.figure()
+			plt.xlabel(L[key])
+			plt.ylabel("Liczba niepoprawnie określonych odległości w optymistycznem przypadku")
+			plt.plot(X[key],Y[key],'go--')
+			plt.savefig(filename.format(key))
+
+
+# In[31]:
+
+
+def loadImage(url):
+	raw = urllib.request.urlopen(url).read()
+	npraw= np.array(bytearray(raw),dtype=np.uint8)
+	return cv2.imdecode(npraw,-1)#-1 -> as is (with the alpha channel)
+
+def getImageName(url):
+	return url.split('/').pop().split('.').pop(0)
+
+def loadImagesFromSite(url):
+	imgs = []
+	print("Loading {} image(s) from {}".format(HOWMANY,url))
+	with urllib.request.urlopen(url) as response:
+		html = BeautifulSoup(response.read(),"lxml")
+		for link in tqdm(html.find_all('a')[:HOWMANY]):
+			img = loadImage(link.get('href'))
+			imgs += [img]
+	return np.array(imgs)  
+
+def saveDataset(t):
+	print("Saving dataset")
+	for key in tqdm(["rotat","scale","blure","jpged"]):
+		with open('pickledDataset_{}'.format(key),'wb') as f:
+			pickle.dump(t[key],f)
+		
+def loadDataset():
+	try:
+		t = {}
+		print("Loading dataset")
+		for key in tqdm(["rotat","scale","blure","jpged"]):
+			with open('pickledDataset_{}'.format(key),'rb') as f:
+				t[key] = pickle.load(f)
+		return t
+	except:
+		print("Load failed")
+		return None
+	
+def getDataset():
+	t = loadDataset()
+	if t is None or DEBUG:
+		images = loadImagesFromSite(URL)
+		points = generatePoints(images)
+		t = getTransformations(images,points)
+		
+		saveDataset(t)
+	return t
+	
+def display(X,lset,howmany=None):
+	if howmany is None:
+		howmany = len(X[lset])
+	
+	print("Preparing images to display")
+	for i in tqdm(range(howmany)):
+		img = X[lset][i]["img"]
+		pp  = X[lset][i]["points"]
+		c = (img.max(),img.max(),img.max())
+		plt.figure()
+		plt.imshow(img)
+	plt.show()	
+		
+
+
+# In[32]:
+
+
+if __name__ =="__main__":
+	t = getDataset()
+	
+	# display(t,"rotat")
+	
+	X,Y = getData(t)
+	L = {}
+	L["rotat"] = "Kąt rotacji (stopnie)"
+	L["scale"] = "Przeskalowanie"
+	L["blure"] = "Odchylenie standardowe"
+	L["jpged"] = "Jakość JPEG"
+	drawPlots(X,Y,L)
+	
+	
---
+++ descriptor.py
@@ -0,0 +1,195 @@
+
+# coding: utf-8
+
+
+# get_ipython().run_line_magic('matplotlib', 'inline')
+import numpy as np
+import matplotlib.pyplot as plt
+import skimage.data as data
+import skimage.color as color
+from skimage.filters import gaussian, scharr
+from skimage.draw import circle,line,circle_perimeter
+from skimage.transform import resize
+import skimage
+import cv2
+import os
+import pickle
+
+D = 5
+
+
+# In[32]:
+
+def getCircle(image,radius):
+	mask = np.zeros(image.shape)
+	mask[circle(image.shape[0]/2,image.shape[1]/2,radius)] = [1,1,1]
+	circled = image*mask
+	#czy to po prostu nie zeruje zer?
+	#DJ: nie, to usuwa puste wiersze/kolumny
+	circled = circled[:,~np.all(np.all(circled==0,axis=2),axis=0)]
+	circled = circled[~np.all(np.all(circled==0,axis=2),axis=1),:]
+	radius = np.ceil(radius)
+	return resize(circled,(2*radius,2*radius,3)),radius
+
+def getPatch(image,center):
+	half_side = 32
+	return image[int(center[0])-half_side:int(center[0])+half_side,int(center[1])-half_side:int(center[1])+half_side]
+
+def getRadius(patch):
+	#return 32 
+	#naprawiłem, jakieś kombinowanie tutaj może raczej tylko zaszkodzić, skoro mamy gwarancję, że tle się zmieści zawsze
+	#DJ: usunąłem rzeczoną poprawkę - tu chodzi o skalę 
+	epsilon = 0.0005
+	side = np.min([patch.shape[0],patch.shape[1]])/2
+	edges = scharr(patch[:,:,1])
+	p = np.where(edges<=epsilon)
+	Ys = p[0] - side
+	Xs = p[1] - side
+	Rs = Ys**2 + Xs**2
+	Rs = Rs[Rs>16]
+	if len(Rs)>0:
+		r = np.floor(np.sqrt(Rs.min()) * 3)
+		return np.min([side,r])
+	else:
+		return side
+
+def getRadiallySymetricalPoints(k1,k2,radius):
+	filename = "points{}_{}".format(k1,k2)
+	if os.path.isfile(filename):
+		with open(filename,'rb') as f:
+			s = pickle.load(f)
+	else:
+		radii = 0.8*np.random.random(k1) + 0.1
+		radii = np.array([np.full((k2),i) for i in radii]).reshape((k2*k1))
+		tmp = np.arange(k2)/k2
+		thetas = np.array([(np.random.random() + tmp) for i in range(k1)]).reshape((k2*k1))
+		thetas *= 2*np.pi
+		ys1 = radii * np.sin(thetas)
+		xs1 = radii * np.cos(thetas)
+		ys2 = radii * np.sin(np.pi + thetas)
+		xs2 = radii * np.cos(np.pi + thetas)
+		s   = np.dstack((ys1,xs1,ys2,xs2))[0]+1
+		
+		with open(filename,'wb') as f:
+			pickle.dump(s,f)
+	return (radius*s).astype(np.int8)
+
+def binarizeImage(image,points):
+	return np.array([0 if image[y1,x1]<image[y2,x2] else 1 for y1,x1,y2,x2 in points])
+
+def gradientDes(img):
+	laplacian = cv2.Laplacian(img,cv2.CV_64F)
+#	 plt.figure()
+#	 plt.imshow(laplacian)
+	
+def rectangleDes(img):
+	r=len(img)
+	im=np.zeros((r//2,len(circle_perimeter(r//2,r//2,r//2)[0]),3))
+	for i in range(r//2):
+		im[i] = resize(img[circle_perimeter(r//2,r//2,i)], im.shape[1:])
+#	 plt.figure()
+#	 plt.imshow(im)
+	dMax = np.zeros((r//2,3),dtype=int)
+	dMin = np.zeros_like(dMax)
+	eMax = np.zeros((len(im[0]),3),dtype=int)
+	eMin = np.zeros_like(eMax)
+	for i in range(r//2):
+		dMax[i] = [np.argmax(im[i,:,x]) for x in range(3)]
+		dMin[i] = [np.argmin(im[i,:,x]) for x in range(3)]
+	for i in range(len(im[0])):
+		eMax[i] = [np.argmax(im[:,i,x]) for x in range(3)]
+		eMin[i] = [np.argmin(im[:,i,x]) for x in range(3)]
+	return dMax, dMin, eMax, eMin
+
+def findDiff(d1,d2,s):
+	nd1 = np.zeros_like(d1)
+	nd2 = np.zeros_like(d2)
+	x = 0
+	for i in range(len(nd1)):
+		nd1[i] = d1[int(x)]
+		nd2[i] = d2[int(x)]
+		x+=s
+	s = np.sum(np.absolute(d1-nd2))
+	return min(s, np.sum(np.absolute(nd1-d2)))
+	
+def compareRectangleDes(d1, d2):
+	m=min(d1.min(), d2.min())
+	d1 -= m
+	d2 -= m
+	m=max(d1.max(), d2.max())	
+	r=np.sum(np.absolute(d1-d2))
+	d=m/3*len(d1)*len(d1[0])
+	for z in np.linspace(0.65,1,7):
+		for i in range(m):
+			d1 = (d1+1)%m
+			s = findDiff(d1,d2,z)
+			if s < r:
+				r = s
+	return min(r/d,1)
+
+def distanceHLPR(r1,r2):
+	return np.abs(r1-r2).sum()
+
+def compareTwoBinaryRings(r1,r2):
+	h1,w1 = r1.shape[:2]
+	h2,w2 = r2.shape[:2]
+	
+	if w1!=w2 and h1!=h2:
+		return np.inf
+	r_max = 0
+	d_max = 0
+	for i in range(h1):
+		d_max += distanceHLPR(r1[i],r2[i])
+	
+	for r in range(1,len(r1) + 1):
+		d = 0
+		for i in range(h1):
+			d += distanceHLPR(r1[i],np.roll(r2[i],r))
+		if d<d_max:
+			d_max = d
+			r_max = r
+	return d_max/w1/h1
+
+
+# In[33]:
+
+
+def extractForKeypoint(image,keypoint):
+	k = 15
+	patch = getPatch(image,keypoint)
+	radius = getRadius(patch)
+	circled,radius = getCircle(patch,radius)
+	points = getRadiallySymetricalPoints(3,12,radius)
+	binary0 = binarizeImage(circled[...,0],points)
+	binary1 = binarizeImage(circled[...,1],points)
+	binary2 = binarizeImage(circled[...,2],points)
+	rMax,rMin,eMax,eMin = rectangleDes(circled)
+	gradientDes(circled)
+	return [np.array([binary0, binary1, binary2]), rMax, rMin, eMax, eMin]
+	# return {0:np.array([binary0, binary1, binary2]), 1:rMax, 2:rMin, 3:eMax, 4:eMin}
+
+def extract(image, keypoints):
+	blur_sigma = 3
+	image = color.rgb2hsv(image)
+	#rozmywać coraz mocniej od środka?
+	blurred = gaussian(image,sigma=blur_sigma)
+	blurred = (blurred - blurred.mean())/blurred.std()#normalizacja
+	blurred[blurred<=-3] = -3#usuwanie outlierów
+	blurred[blurred>=3] = 3
+	blurred = (blurred - blurred.min()) / (blurred.max() - blurred.min())#normalizacja taka, by można to było wyświetlić
+	return [extractForKeypoint(blurred,keypoint) for keypoint in keypoints]
+
+def distance(descriptor1, descriptor2):
+	return 1
+	
+if __name__=="__main__":
+	image = data.astronaut()[:-1,:-1]
+	plt.imshow(image)
+	w=extract(image,[[100,100],[121,101],[100,99]])
+	print("binary",	compareTwoBinaryRings(w[0][0],w[1][0]), compareTwoBinaryRings(w[0][0],w[2][0]))
+#	 print("rectangle", compareRectangleDes(w[0][5], w[1][5]), compareRectangleDes(w[0][5], w[2][5]))
+#	 print("rectangle", compareRectangleDes(w[0][5], w[1][5]), compareRectangleDes(w[0][5], w[2][5]))
+#	 print("rectangle", compareRectangleDes(w[0][6], w[1][6]), compareRectangleDes(w[0][6], w[2][6]))
+	
+	
+
